dbac386460dbfeafd0402bcd39855217c4bbbf37
mokhtarjo21 <mokhtar.jo21@gmail.com>
2025-12-14 21:40:36 +0200

    edite LanguageToggle butten

--- src/components/ui/LanguageToggle.tsx
+++ src/components/ui/LanguageToggle.tsx
@@ -16,7 +16,7 @@ export const LanguageToggle: React.FC = () => {
   };
 
   const getCurrentLanguageCode = () => {
-    return i18n.language === 'ar' ? 'EN' : 'عر';
+    return i18n.language === 'ar' ? 'EN' : 'AR';
   };
 
   return (
